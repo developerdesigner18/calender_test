284541e648c5e3869143d4b27a6134bedfa19fff
developer.designer18@gmail.com <developer.designer18@gmail.com>
2021-08-23 15:14:08 +0530

    intial commit 2

--- src/app/calender/calender.module.ts
+++ src/app/calender/calender.module.ts
@@ -1,27 +1,20 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CalenderComponent } from './calender/calender.component';
-import { NgbModalModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { FlatpickrModule } from 'angularx-flatpickr';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { CalenderRoutingModule } from './calender-routing.module';
+import { NgbModalModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
- 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CalenderComponent,
-  ],
+  declarations: [],
   imports: [
-    BrowserModule,
+    CommonModule,
+    CalenderRoutingModule,
     BrowserAnimationsModule,
-    AppRoutingModule,
     NgbModule,
     CommonModule,
     FormsModule,
@@ -31,9 +24,8 @@ import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
       provide: DateAdapter,
       useFactory: adapterFactory,
     }),
-  ],
-  exports: [CalenderComponent],
-  providers: [],
-  bootstrap: [AppComponent]
+  ]
 })
-export class AppModule { }
+export class CalenderModule { }
+
+
